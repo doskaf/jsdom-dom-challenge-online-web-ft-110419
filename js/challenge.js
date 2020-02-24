d68e06e7448364a1f03b0fd857a365030d5e146f
Dominique Skaf <doskaf@yahoo.com>
2020-02-24 17:11:10 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -52,5 +52,6 @@ document.getElementById("heart").addEventListener("click",function() {
 });
 
 document.getElementById("pause").addEventListener("click",function() {
-  
+  let pause = document.getElementById("pause");
+  if pause.innerText = "pause"
 });
